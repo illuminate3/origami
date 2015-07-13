b1c5c4b1a565b95353872b8a452088dfbee95140
illuminate3 <carsten.sapporo@gmail.com>
2015-07-12 19:21:13 -0500

    dev 5.4

--- Resources/Views/welcome/origami.blade.php
+++ Resources/Views/welcome/origami.blade.php
@@ -21,7 +21,7 @@
 	<div class="container">
 		<div class="content">
 			<a href="/">
-				<img src="/assets/images/origami.jpg">
+				<img src="/assets/images/origami.jpg" class="img-responsive">
 			</a>
 			<div class="title">
 				<a href="/">
